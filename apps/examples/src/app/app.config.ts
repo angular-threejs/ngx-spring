e7815f06d2bf41ef8a69d218316e20375d71c91f
Chau <25516557+nartc@users.noreply.github.com>
2026-01-11 15:59:18 +0700

    chore: setup

--- apps/examples/src/app/app.config.ts
+++ apps/examples/src/app/app.config.ts
@@ -1,18 +1,18 @@
 import {
-  ApplicationConfig,
-  provideBrowserGlobalErrorListeners,
+	ApplicationConfig,
+	provideBrowserGlobalErrorListeners,
 } from '@angular/core';
+import {
+	provideClientHydration,
+	withEventReplay,
+} from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
-import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideClientHydration(withEventReplay()),
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(appRoutes),
-  ],
+	providers: [
+		provideClientHydration(withEventReplay()),
+		provideBrowserGlobalErrorListeners(),
+		provideRouter(appRoutes),
+	],
 };
--- apps/examples/src/app/app.spec.ts
+++ apps/examples/src/app/app.spec.ts
@@ -3,18 +3,18 @@ import { App } from './app';
 import { NxWelcome } from './nx-welcome';
 
 describe('App', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [App, NxWelcome],
-    }).compileComponents();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [App, NxWelcome],
+		}).compileComponents();
+	});
 
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome examples',
-    );
-  });
+	it('should render title', async () => {
+		const fixture = TestBed.createComponent(App);
+		await fixture.whenStable();
+		const compiled = fixture.nativeElement as HTMLElement;
+		expect(compiled.querySelector('h1')?.textContent).toContain(
+			'Welcome examples',
+		);
+	});
 });
